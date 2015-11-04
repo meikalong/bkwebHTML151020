046f60fd870d3f1682966ee9727545567e94cd20
xiaolanglang <1712877559@qq.com>
2015-11-04 16:10:38 +0800

    加入图片放大查看

--- sass/js/company/main.js
+++ sass/js/company/main.js
@@ -30,18 +30,28 @@ require.config({
 		//响应式导航
 		"responsive_nav": ["./common/responsive-nav"],
 		// 方向悬停特效
-		"hoverdir": ["./common/jquery.hoverdir"]
+		"hoverdir": ["./common/jquery.hoverdir"],
+		//全屏图片查看
+		"chocolat": ["./common/jquery.chocolat"]
 	},
 	shim: {
 		'hoverdir': {
 			deps: ["jquery"],
 			exports: 'hoverdir'
+		},
+		'chocolat': {
+			deps: ["jquery"],
+			exports: 'chocolat'
 		}
 	}
 });
 
-require(["jquery", 'angular', 'ui-router', "app", "routes", "responsive_nav", "hoverdir"], function($, angular, a) {
+require(["jquery", 'angular', 'ui-router', "app", "routes",
+	"responsive_nav", "hoverdir", "chocolat"
+], function($, angular, a) {
 	$(function() {
+		// 启动angularjs
+		angular.bootstrap(document, ["app"]);
 		//导入公共导航栏
 		$(".nav_menu").load("./common/index_top.html", function() {
 			//创建响应式菜单
@@ -50,9 +60,14 @@ require(["jquery", 'angular', 'ui-router', "app", "routes", "responsive_nav", "h
 			});
 		});
 		// 创建方向悬停特效
-		$('#da-thumbs > li').hoverdir();
-		// 启动angularjs
-		angular.bootstrap(document, ["app"]);
+		$('#da-thumbs li').hoverdir();
+		// 创建图片全屏查看
+		$('#da-thumbs a').Chocolat({
+			leftImg:"../../img/chocolat/left.png",
+			rightImg:"../../img/chocolat/right.png",
+			closeImg:"../../img/chocolat/close.png",
+			loadingImg:"../../img/chocolat/loading.gif",
+		});
 	})
 
 })
